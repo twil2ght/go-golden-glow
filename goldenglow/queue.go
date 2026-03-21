4483ec0fb52c644b9162a7973e8fa6cb6c065702
twilight <2359643864@qq.com>
2026-03-21 20:58:15 +0800

    feat: default queue

---
+++ goldenglow/queue.go
@@ -0,0 +1,22 @@
+package goldenglow
+
+type queue struct {
+	ch chan string
+}
+
+func (q *queue) In(s string) error {
+	if s == "" {
+		return nil
+	}
+	q.ch <- s
+	return nil
+}
+
+func (q *queue) Out() <-chan string {
+	return q.ch
+}
+func NewQueue() Queue {
+	return &queue{
+		ch: make(chan string),
+	}
+}
