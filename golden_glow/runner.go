5c1ad217fa2051e26cac9ae5704314e664fc8c89
twilight <2359643864@qq.com>
2026-03-21 12:12:12 +0800

    refractor: handler errors in New()

--- golden_glow/runner.go
+++ golden_glow/runner.go
@@ -75,20 +75,56 @@ func (b *Base) SetTemplateCore(f template.Core) error {
 }
 
 func New(logger log.Logger, db storage.Repository) Engine {
-	//Default Config
+	// Default Config
 	var (
-		variableParser  = variable.ToRawText
-		nRegulator      = node.NewRegulator(storage.NewKVLite(db))
-		nodeFactory     = node.NewFactory(variableParser, nRegulator)
-		fetcher, err    = container.NewFetcher(db, nodeFactory)
-		nEncoder        = node.DefaultEncoder()
-		positioner, err = container.NewPositioner(db, nEncoder)
+		variableParser = variable.ToRawText
+		nEncoder       = node.DefaultEncoder()
 
-		//TODO templateSource
-		templateCore = template.New(nil, variable.VarReg)
-		//TODO err
-		containerFactory, _ = container.NewFactory(fetcher, nEncoder, positioner)
+		nRegulator       node.Regulator
+		nodeFactory      node.Factory
+		fetcher          container.Fetcher
+		positioner       container.Positioner
+		templateCore     template.Core
+		containerFactory container.Factory
+		err              error
 	)
+
+	nRegulator, err = node.NewRegulator(storage.NewKVLite(db))
+	if err != nil {
+		logger.Error("failed to create node regulator", err)
+		panic(err)
+	}
+
+	nodeFactory, err = node.NewFactory(variableParser, nRegulator)
+	if err != nil {
+		logger.Error("failed to create node factory", err)
+		panic(err)
+	}
+
+	fetcher, err = container.NewFetcher(db, nodeFactory)
+	if err != nil {
+		logger.Error("failed to create fetcher", err)
+		panic(err)
+	}
+
+	positioner, err = container.NewPositioner(db, nEncoder)
+	if err != nil {
+		logger.Error("failed to create positioner", err)
+		panic(err)
+	}
+
+	templateCore, err = template.New(nil, variable.VarReg)
+	if err != nil {
+		logger.Error("failed to create template core", err)
+		panic(err)
+	}
+
+	containerFactory, err = container.NewFactory(fetcher, nEncoder, positioner)
+	if err != nil {
+		logger.Error("failed to create container factory", err)
+		panic(err)
+	}
+
 	return &Base{
 		Logger:           logger,
 		containerFactory: containerFactory,
