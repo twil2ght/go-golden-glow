1e7f36de3c1e8723b6086832948bd445f2378733
twilight <2359643864@qq.com>
2026-03-22 12:13:25 +0800

    feat: database repository start

--- storage/database.go
+++ storage/database.go
@@ -5,17 +5,35 @@ import (
 	"goldenglow/m"
 )
 
-// DBRepository TODO implement
-type DBRepository struct {
+// dbRepository TODO implement
+type dbRepository struct {
 	db *sql.DB
 }
 
-func (D *DBRepository) HGet(tag string) (m.Hash, error) {
+func (D *dbRepository) Get(key string) (string, error) {
 	//TODO implement me
 	panic("implement me")
 }
 
-func (D *DBRepository) HSet(tag string, value m.Hash) error {
+func (D *dbRepository) Set(key, value string) error {
 	//TODO implement me
 	panic("implement me")
+}
+
+func (D *dbRepository) Shutdown() error {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (D *dbRepository) HGet(tag string) (m.Hash, error) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (D *dbRepository) HSet(tag string, value m.Hash) error {
+	//TODO implement me
+	panic("implement me")
+}
+func NewDBRepository(db *sql.DB) Repository {
+	return &dbRepository{db: db}
 }
