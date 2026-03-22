baf13e7e18ab05f9127dab4014b3dd362fc39a65
twilight <2359643864@qq.com>
2026-03-22 12:08:54 +0800

    refractor:move kvLite to single file

--- storage/model.go
+++ storage/model.go
@@ -19,30 +19,3 @@ type LightRepository interface {
 	Get(key string) (string, error)
 	Set(key, value string) error
 }
-type kvLite struct {
-	lr LightRepository
-}
-
-const (
-	prefixKeyOf = "KeyOf:"
-)
-
-func (kv *kvLite) Get(key string) (string, error) {
-	return kv.lr.Get(key)
-}
-func (kv *kvLite) Set(key, value string) error {
-	// 存 key → value
-	if err := kv.lr.Set(key, value); err != nil {
-		return err
-	}
-	// 存 value → key（反向映射）
-	return kv.lr.Set(prefixKeyOf+value, key)
-}
-func (kv *kvLite) Keyof(value string) (string, error) {
-	return kv.Get(prefixKeyOf + value)
-}
-func NewKVLite(repository LightRepository) KVLite {
-	return &kvLite{
-		lr: repository,
-	}
-}
