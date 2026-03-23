e9ff509d93151149903f138760b25128538242d3
twilight <2359643864@qq.com>
2026-03-23 21:26:25 +0800

    feat: DataGen interface implement

---
+++ dataGen/dataGen.go
@@ -0,0 +1,63 @@
+package dataGen
+
+import (
+	"errors"
+	"fmt"
+	"sync"
+)
+
+var (
+	globalRegistry DataGen
+	once           sync.Once
+)
+
+type dataGen struct {
+	genRegistries map[string]Generator
+}
+
+func (d *dataGen) AddGenerator(pluginName string, generator Generator) error {
+	if d.genRegistries == nil {
+		d.genRegistries = make(map[string]Generator)
+	}
+	if pluginName == "" {
+		return errors.New("pluginName is empty")
+	}
+	if generator == nil {
+		return errors.New("generator is nil")
+	}
+	d.genRegistries[pluginName] = generator
+	return nil
+}
+
+func (d *dataGen) RunAll() error {
+	for pluginName, generator := range d.genRegistries {
+		err := generator.Run()
+		if err != nil {
+			return fmt.Errorf("plugin %s generator err: %v", pluginName, err)
+		}
+	}
+	return nil
+}
+
+func initGlobal() {
+	once.Do(func() {
+		globalRegistry = NewDataGen()
+	})
+}
+func NewDataGen() DataGen {
+	return &dataGen{
+		genRegistries: make(map[string]Generator),
+	}
+}
+func AutoRegister(name string, generator Generator) {
+	initGlobal()
+	err := globalRegistry.AddGenerator(name, generator)
+	if err != nil {
+		panic(err)
+	}
+}
+
+func RunAll() error {
+	initGlobal()
+	return globalRegistry.RunAll()
+}
