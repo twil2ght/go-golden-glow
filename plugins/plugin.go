88d1c6d548161e61ccdbb601877132c7bdb4cf12
twilight <2359643864@qq.com>
2026-03-23 21:47:27 +0800

    refractor:better method name

--- plugins/plugin.go
+++ plugins/plugin.go
@@ -1,27 +1,18 @@
 package plugins
 
-import (
-	"goldenglow/plugin"
-)
-
 type basePlugin struct {
 	name string
 }
 
-func (b *basePlugin) Shutdown() {}
+func (b *basePlugin) Cleanup() {}
 
-func (b *basePlugin) Init() {}
+func (b *basePlugin) Setup() {}
 
 func (b *basePlugin) Name() string {
 	return b.name
 }
 
-func (b *basePlugin) Lang() plugin.TRGroup {
-	//TODO implement me
-	panic("implement me")
-}
-
 func (b *basePlugin) ExecuteHandler() ExecuteHandler {
 	//TODO implement me
-	panic("implement me")
+	panic(b.name + ":ExecuteHandler not implemented")
 }
