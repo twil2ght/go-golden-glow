63e43974e085fc16dc5cf463c8a77bf80429e2c2
twilight <2359643864@qq.com>
2026-03-23 21:25:05 +0800

    refractor:move constants to single file

---
+++ dataGen/const.go
@@ -0,0 +1,10 @@
+package dataGen
+
+const (
+	langTypeDefault LangType = "default"
+	langTypeCheck   LangType = "check"
+	langTypeExtract LangType = "extract"
+	RootDir                  = "../../data/"
+	jsonExt                  = ".json"
+	KeyDefault               = "[node]"
+)
