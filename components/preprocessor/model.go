cc35d05acdae49c7240f0ffc5b7eab2ea3ed96aa
twilight <2359643864@qq.com>
2026-03-24 13:35:22 +0800

    refractor:split interface

--- components/preprocessor/model.go
+++ components/preprocessor/model.go
@@ -5,5 +5,7 @@ import "goldenglow/components"
 type Handler func(msg string) string
 type Instance interface {
 	Preprocess(message components.Message) string
+}
+type Registry interface {
 	Register(tag string, handler Handler) error
 }
