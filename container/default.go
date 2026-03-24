2f835eb3ce4b86298891b46f3dc0ca8529597dc6
twilight <2359643864@qq.com>
2026-03-24 17:57:27 +0800

    feat:default store

--- container/default.go
+++ container/default.go
@@ -2,14 +2,19 @@ package container
 
 import (
 	"goldenglow/node"
+	"goldenglow/storage"
 )
 
 var (
-	fetcherInstance, _    = NewFetcher(nil, node.DefaultFactory())
-	positionerInstance, _ = NewPositioner(nil, node.DefaultEncoder())
+	db                    = storage.DefaultJSONRepo()
+	encoder               = node.DefaultEncoder()
+	fetcherInstance, _    = NewFetcher(db, node.DefaultFactory())
+	positionerInstance, _ = NewPositioner(db, encoder)
 	factoryInstance, _    = NewFactory(fetcherInstance, positionerInstance)
+	storeInstance         = NewStore(db, encoder)
 )
 
 func DefaultFactory() Factory {
 	return factoryInstance
 }
+func DefaultStore() Store { return storeInstance }
