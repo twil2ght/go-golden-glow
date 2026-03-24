1985e83666999f87bbdbd68276f14d2f2056cc37
twilight <2359643864@qq.com>
2026-03-24 14:49:03 +0800

    refractor:migrate

--- components/queue/queue.go
+++ components/queue/queue.go
@@ -4,3 +4,23 @@ type Queue interface {
 	In(string) error
 	Out() <-chan string
 }
+type queue struct {
+	ch chan string
+}
+
+func (q *queue) In(s string) error {
+	if s == "" {
+		return nil
+	}
+	q.ch <- s
+	return nil
+}
+
+func (q *queue) Out() <-chan string {
+	return q.ch
+}
+func NewQueue() Queue {
+	return &queue{
+		ch: make(chan string),
+	}
+}
