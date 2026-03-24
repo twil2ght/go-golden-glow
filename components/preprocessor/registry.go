66c4822f621561fd3ebfafec5e96436b7f63293b
twilight <2359643864@qq.com>
2026-03-24 16:12:37 +0800

    feat: impl preprocess register()

--- components/preprocessor/registry.go
+++ components/preprocessor/registry.go
@@ -1,1 +1,22 @@
 package preprocessor
+
+import "errors"
+
+type registry struct {
+	handlers map[string]Handler
+}
+
+func (r *registry) Register(pluginName, tag string, handler Handler) error {
+	if pluginName == "" {
+		return errors.New("plugin name is empty")
+	}
+	if tag == "" {
+		return errors.New("plugin tag is empty")
+	}
+	if handler == nil {
+		return errors.New("source is nil")
+	}
+	key := pluginName + ":" + tag
+	r.handlers[key] = handler
+	return nil
+}
