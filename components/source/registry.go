f546724cf4f04b794d57c6c888729322cd06ee7c
twilight <2359643864@qq.com>
2026-03-24 16:04:24 +0800

    feat: impl source register()

--- components/source/registry.go
+++ components/source/registry.go
@@ -1,5 +1,22 @@
 package source
 
+import "errors"
+
 type registry struct {
 	sources map[string]Source
+}
+
+func (r *registry) Register(pluginName, tag string, source Source) error {
+	if pluginName == "" {
+		return errors.New("plugin name is empty")
+	}
+	if tag == "" {
+		return errors.New("plugin tag is empty")
+	}
+	if source == nil {
+		return errors.New("source is nil")
+	}
+	key := pluginName + ":" + tag
+	r.sources[key] = source
+	return nil
 }
