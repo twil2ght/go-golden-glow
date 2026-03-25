6bdadf29cd3530279956e2ecbabdcf3a08887f0d
twilight <2359643864@qq.com>
2026-03-25 15:37:32 +0800

    feat:NotNull() impled

--- utils/utils.go
+++ utils/utils.go
@@ -1,8 +1,10 @@
 package utils
 
 import (
+	"fmt"
 	"os"
 	"path/filepath"
+	"reflect"
 	"strings"
 )
 
@@ -45,4 +47,43 @@ func findProjectRoot(start string) string {
 	}
 
 	return start
+}
+
+//TODO 对所有含有大量检查的做简化
+
+// NotNull 参数必须成对传入：key1, val1, key2, val2...
+// 只校验 val 是否为 nil / 空字符串
+func NotNull(args ...any) error {
+	if len(args)%2 != 0 {
+		return fmt.Errorf("args should be even,current amount: %d", len(args))
+	}
+
+	for i := 0; i < len(args); i += 2 {
+		key := args[i]
+		val := args[i+1]
+
+		if isNil(val) {
+			return fmt.Errorf("%s is nil", key)
+		}
+
+		if str, ok := val.(string); ok && str == "" {
+			return fmt.Errorf("%s is empty", key)
+		}
+	}
+
+	return nil
+}
+
+// isNil 安全判断 nil（兼容 error 等接口类型）
+func isNil(a any) bool {
+	if a == nil {
+		return true
+	}
+	v := reflect.ValueOf(a)
+	switch v.Kind() {
+	case reflect.Ptr, reflect.Interface, reflect.Slice, reflect.Map, reflect.Chan, reflect.Func:
+		return v.IsNil()
+	default:
+		return false
+	}
 }
