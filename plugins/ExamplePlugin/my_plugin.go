4341ac60387c0a96350e76cc3d871320db8f658a
twilight <2359643864@qq.com>
2026-03-25 22:00:57 +0800

    fix:example plugin repaired

--- plugins/ExamplePlugin/my_plugin.go
+++ plugins/ExamplePlugin/my_plugin.go
@@ -1,50 +1,62 @@
-package ExamplePlugin
+package exampleplugin
 
 import (
 	"fmt"
 	"goldenglow/dataGen"
 	"goldenglow/executor"
+	"goldenglow/lang"
 	"goldenglow/plugins"
 )
 
 func init() {
-	err := plugins.Subscribe(NewMyPlugin())
+	err := plugins.Subscribe(NewExamplePlugin())
 	if err != nil {
 		panic(err)
 	}
 }
 
-type myPlugin struct {
+const (
+	pluginName = "example"
+)
+
+type examplePlugin struct {
 	plugins.Base
 }
 
-func (m *myPlugin) OnRegisterExecutor(reg executor.Registry) error {
-	return reg.Register(m.Name(), func(params executor.Parameters) error {
-		var (
-			response = params["response"]
-		)
+func (e *examplePlugin) Name() string {
+	return pluginName
+}
+
+func (e *examplePlugin) OnRegisterExecutor(reg executor.Registry) error {
+	return reg.Register(e.Name(), func(params executor.Parameters) error {
+		response := params["response"]
 		if response == "" {
-			return fmt.Errorf("not found response")
+			return fmt.Errorf("example plugin: not found response parameter")
 		}
-		fmt.Println(response)
+
+		fmt.Printf("[ExamplePlugin] got response: %s\n", response)
 		return nil
 	})
 }
 
-func (m *myPlugin) OnRegisterDataGen(reg dataGen.Registry) error {
-	var (
-		generator = dataGen.NewGenerator(m.Name())
-	)
+func (e *examplePlugin) OnRegisterDataGen(reg dataGen.Registry) error {
+	generator := dataGen.NewGenerator(e.Name())
+
 	generator.Add("Hello", dataGen.New(
 		[]string{"Hello $1"},
 		dataGen.Parameters{
-			"response": "World",
+			"response": "$1",
 		},
 		dataGen.LangTypeDefault,
 	))
-	return reg.AddGenerator(m.Name(), generator)
+
+	return reg.AddGenerator(e.Name(), generator)
 }
 
-func NewMyPlugin() plugins.Item {
-	return &myPlugin{}
+func (e *examplePlugin) OnRegisterLang(reg lang.Registry) error {
+	return reg.Register(pluginName)
+}
+
+func NewExamplePlugin() plugins.Item {
+	return &examplePlugin{}
 }
