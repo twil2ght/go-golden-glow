b678b325515d0ca2d6ecba3d7afe823f7a40d004
twilight <2359643864@qq.com>
2026-03-25 13:30:37 +0800

    feat:impl builder

--- plugins/builtin/builder/builder.go
+++ plugins/builtin/builder/builder.go
@@ -1,14 +1,18 @@
 package builder
 
 import (
+	"fmt"
+	"goldenglow/config"
+	"goldenglow/container"
 	"goldenglow/dataGen"
 	"goldenglow/executor"
 	"goldenglow/lang"
+	"goldenglow/m"
 	"goldenglow/plugins"
 )
 
 func init() {
-	if err := plugins.Subscribe(NewBuilderPlugin()); err != nil {
+	if err := plugins.Subscribe(NewBuilderPlugin(container.DefaultStore())); err != nil {
 		panic(err)
 	}
 }
@@ -33,14 +37,66 @@ var (
 
 type builder struct {
 	plugins.Base
+	saver      container.Store
+	pocketCs2R []string
+	pocketC2Rs []string
 }
 
 func (b *builder) Name() string {
 	return pluginName
 }
 
-func (b *builder) add(value, valueType, mode string) {
-	//	TODO implement me
+func (b *builder) add(value, valueType, mode string) error {
+	switch mode {
+	case modeMultiCondition:
+		b.pocketCs2R = append(b.pocketCs2R, value)
+	case modeMultiResult:
+		b.pocketC2Rs = append(b.pocketC2Rs, value)
+	}
+
+	if valueType == typeOutput {
+		return b.build(mode)
+	}
+	return nil
+}
+
+func (b *builder) build(mode string) error {
+	switch mode {
+	case modeMultiCondition:
+		return b.buildMultiCondition()
+	case modeMultiResult:
+		return b.buildMultiResult()
+	default:
+		return fmt.Errorf("unsupported mode: %s", mode)
+	}
+}
+
+func (b *builder) buildMultiCondition() error {
+	defer func() { b.pocketCs2R = nil }()
+	data := b.pocketCs2R
+
+	if len(data) < 2 {
+		return fmt.Errorf("pocketCs2R length < 2, data: %v", data)
+	}
+
+	conditions := data[:len(data)-1]
+	result := []string{data[len(data)-1]}
+
+	return b.saver.Save(m.ToHash(conditions), m.ToHash(result))
+}
+
+func (b *builder) buildMultiResult() error {
+	defer func() { b.pocketC2Rs = nil }()
+	data := b.pocketC2Rs
+
+	if len(data) < 2 {
+		return fmt.Errorf("pocketC2Rs length < 2, data: %v", data)
+	}
+
+	condition := []string{data[len(data)-1]}
+	results := data[:len(data)-1]
+
+	return b.saver.Save(m.ToHash(condition), m.ToHash(results))
 }
 
 func (b *builder) OnRegisterExecutor(reg executor.Registry) error {
@@ -48,25 +104,46 @@ func (b *builder) OnRegisterExecutor(reg executor.Registry) error {
 		if err := executor.Validate(params, keyValue, keyType, keyMode); err != nil {
 			return err
 		}
-
-		var (
-			value     = params[keyValue]
-			mode      = params[keyMode]
-			valueType = params[keyType]
-		)
-
-		b.add(value, valueType, mode)
-
-		return nil
+		return b.add(params[keyValue], params[keyType], params[keyMode])
 	})
 }
 
 func (b *builder) OnRegisterDataGen(reg dataGen.Registry) error {
 	generator := dataGen.NewGenerator(b.Name())
-	//TODO implement me:need 4 langItems
-	generator.Add(modeMultiCondition, dataGen.New(
-		[]string{},
-		dataGen.Parameters{},
+	generator.Add("input_Cs", dataGen.New(
+		[]string{fmt.Sprintf("%s says [input_Cs] $1 to %s", config.User, config.GG)},
+		dataGen.Parameters{
+			keyValue: "$1",
+			keyMode:  modeMultiCondition,
+			keyType:  typeInput,
+		},
+		dataGen.LangTypeDefault,
+	))
+	generator.Add("output_Cs", dataGen.New(
+		[]string{fmt.Sprintf("%s says [output_Cs] $1 to %s", config.User, config.GG)},
+		dataGen.Parameters{
+			keyValue: "$1",
+			keyMode:  modeMultiCondition,
+			keyType:  typeOutput,
+		},
+		dataGen.LangTypeDefault,
+	))
+	generator.Add("input_Rs", dataGen.New(
+		[]string{fmt.Sprintf("%s says [input_Rs] $1 to %s", config.User, config.GG)},
+		dataGen.Parameters{
+			keyValue: "$1",
+			keyMode:  modeMultiResult,
+			keyType:  typeInput,
+		},
+		dataGen.LangTypeDefault,
+	))
+	generator.Add("output_Rs", dataGen.New(
+		[]string{fmt.Sprintf("%s says [output_Rs] $1 to %s", config.User, config.GG)},
+		dataGen.Parameters{
+			keyValue: "$1",
+			keyMode:  modeMultiResult,
+			keyType:  typeOutput,
+		},
 		dataGen.LangTypeDefault,
 	))
 
@@ -77,6 +154,8 @@ func (b *builder) OnRegisterLang(reg lang.Registry) error {
 	return reg.Register(pluginName)
 }
 
-func NewBuilderPlugin() plugins.Item {
-	return &builder{}
+func NewBuilderPlugin(saver container.Store) plugins.Item {
+	return &builder{
+		saver: saver,
+	}
 }
