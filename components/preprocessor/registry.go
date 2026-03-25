bb60767b836f2b2286c7aedbfa20675804f8bc47
twilight <2359643864@qq.com>
2026-03-25 21:38:05 +0800

    feat:preprocessor registry logger impled

--- components/preprocessor/registry.go
+++ components/preprocessor/registry.go
@@ -1,23 +1,29 @@
 package preprocessor
 
-import "errors"
+import (
+	"goldenglow/pkg/log"
+	"goldenglow/utils"
+)
+
+var (
+	logger = log.Default()
+)
 
 type registry struct {
 	handlers map[string]Handler
 }
 
 func (r *registry) Register(pluginName, tag string, handler Handler) error {
-	if pluginName == "" {
-		return errors.New("plugin name is empty")
-	}
-	if tag == "" {
-		return errors.New("plugin tag is empty")
-	}
-	if handler == nil {
-		return errors.New("source is nil")
+	if err := utils.NotNull(
+		"pluginName", pluginName,
+		"tag", tag,
+		"handler", handler,
+	); err != nil {
+		return err
 	}
 	key := pluginName + ":" + tag
 	r.handlers[key] = handler
+	logger.Info("Preprocessor: Register preprocessor", "pluginName", pluginName, "tag", tag)
 	return nil
 }
 func NewRegistry() Registry {
