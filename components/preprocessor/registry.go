31d7ca9c3bdf12139160266477c97ad5bdb1745a
twilight <2359643864@qq.com>
2026-03-26 22:56:54 +0800

    feat(preprocessor): add Preprocess method to handle message processing
    - Implement Preprocess function to process messages based on their tags
    - Retrieve message value and tag inside Preprocess method
    - Call corresponding handler if tag exists in the registry
    - Return original value if no handler for the tag is found

--- components/preprocessor/registry.go
+++ components/preprocessor/registry.go
@@ -1,6 +1,7 @@
 package preprocessor
 
 import (
+	"goldenglow/components"
 	"goldenglow/pkg/log"
 	"goldenglow/utils"
 )
@@ -11,6 +12,17 @@ var (
 
 type registry struct {
 	handlers map[string]Handler
+}
+
+func (r *registry) Preprocess(message components.Message) string {
+	var (
+		value = message.Value()
+		tag   = message.Tag()
+	)
+	if handler, ok := r.handlers[tag]; ok {
+		return handler(value)
+	}
+	return value
 }
 
 func (r *registry) Register(pluginName, tag string, handler Handler) error {
