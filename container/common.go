bcc15c9cfe22f473dd4f57a350e7dd9e8abd22b7
twilight <2359643864@qq.com>
2026-03-27 21:50:57 +0800

    fix(container): correct variable token extraction and validation logic
    - Remove unnecessary length and boundary checks for dMatch and tMatches
    - Fix token assignment to use dMatch[0] instead of dMatch[1]
    - Fix varKey assignment to use tMatches[index][0] instead of tMatches[index][1]
    - Add debug logging for previous variable retrieval
    - Return not found error if previous variable is nil

--- container/common.go
+++ container/common.go
@@ -1,7 +1,6 @@
 package container
 
 import (
-	"fmt"
 	"goldenglow/node"
 	"goldenglow/pkg/log"
 	"goldenglow/variable"
@@ -15,24 +14,12 @@ func variableGen(
 	T node.Item,
 	targetVars variable.Set,
 ) error {
-	if len(dMatch) < 2 {
-		return nil
-	}
-
 	// 目标变量名
-	token := dMatch[1]
-	if !variable.Is(token) {
-		return nil
-	}
-
-	// 越界检查
-	if index >= len(tMatches) || len(tMatches[index]) < 2 {
-		return fmt.Errorf("invalid variable at index %d", index)
-	}
-
+	token := dMatch[0]
 	// 源变量名
-	varKey := tMatches[index][1]
+	varKey := tMatches[index][0]
 	prevVar := T.Variables()[varKey]
+	logger.Debug("prevVar", "", prevVar)
 	if prevVar == nil {
 		return log.NotFound("variable:" + varKey)
 	}
