ee02fd37ea0043c9870c45ace1e72ef06fbfc520
twilight <2359643864@qq.com>
2026-04-06 08:37:37 +0800

    refactor(runner): move Knot interface and implementation to knot.go
    - Extract Knot interface and knot struct from runner.go to knot.go
    - Implement getter and setter methods for Knot in knot.go
    - Move NewKnot constructor function to knot.go
    - Remove Knot related code from runner.go to improve code organization and separation
    - Update imports accordingly in knot.go and runner.go

--- components/runner/runner.go
+++ components/runner/runner.go
@@ -15,51 +15,6 @@ import (
 
 var logger = log.Default()
 
-type Knot interface {
-	Trigger() node.Item
-	Trace() m.Hash
-	TreeNode() *TreeNode
-	SetTreeNode(node *TreeNode)
-}
-type knot struct {
-	trigger  node.Item
-	trace    m.Hash
-	treeNode *TreeNode
-}
-
-func (d *knot) Trigger() node.Item {
-	return d.trigger
-}
-func (d *knot) Trace() m.Hash {
-	return d.trace
-}
-func (d *knot) TreeNode() *TreeNode {
-	return d.treeNode
-}
-func (d *knot) SetTreeNode(node *TreeNode) {
-	d.treeNode = node
-}
-func NewKnot(t, src node.Item, trace m.Hash, encoder node.Encoder) (Knot, error) {
-	if t == nil {
-		return nil, fmt.Errorf("NewKnot: trigger==nil")
-	}
-	if trace == nil {
-		trace = m.Hash{}
-	}
-	var (
-		nodeValue = t.Value()
-		encoded   = encoder.Do(nodeValue)
-	)
-	if _, ok := trace[encoded]; ok && src == nil {
-		return nil, errors.New("duplicate node")
-	}
-	trace[encoded] = struct{}{}
-	return &knot{
-		trigger: t,
-		trace:   trace,
-	}, nil
-}
-
 type Instance interface {
 	Run(node node.Item) error
 }
