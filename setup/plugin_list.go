464468b01325b5d1b0b172df238f7adc71495366
twilight <2359643864@qq.com>
2026-04-16 21:53:25 +0800

    refactor(setup): move plugin imports to plugin_list.go
    - Removed builtin plugin imports from setup.go
    - Added a new file plugin_list.go to hold builtin plugin imports
    - Improved code organization by centralizing plugin registrations in one file
    - Maintained same plugin set: builder, calculator, simplekv, speaker, timer

---
+++ setup/plugin_list.go
@@ -0,0 +1,9 @@
+package setup
+
+import (
+	_ "goldenglow/plugins/builtin/builder"
+	_ "goldenglow/plugins/builtin/calculator"
+	_ "goldenglow/plugins/builtin/simplekv"
+	_ "goldenglow/plugins/builtin/speaker"
+	_ "goldenglow/plugins/builtin/timer"
+)
--- setup/setup.go
+++ setup/setup.go
@@ -10,11 +10,6 @@ import (
 	"goldenglow/executor/extractor"
 	"goldenglow/lang"
 	"goldenglow/plugins"
-	_ "goldenglow/plugins/builtin/builder"
-	_ "goldenglow/plugins/builtin/calculator"
-	_ "goldenglow/plugins/builtin/simplekv"
-	_ "goldenglow/plugins/builtin/speaker"
-	_ "goldenglow/plugins/builtin/timer"
 	"goldenglow/storage"
 )
 
