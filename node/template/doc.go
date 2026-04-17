cca3f45d450e33371e533ae6ca8c32948e693734
twilight <2359643864@qq.com>
2026-04-17 08:53:58 +0800

    docs(template): add initial package documentation for template package
    - Provide overview of the template package purpose
    - Specify that the template "$1 is $2" is prohibited as the sole template
    - Create documentation stub for future expansion

---
+++ node/template/doc.go
@@ -0,0 +1,4 @@
+// Package template provides a set of templates for generating code.
+// the template "$1 is $2" is the only template that is prohibited
+// (can be a template when it is the only one)
+package template
