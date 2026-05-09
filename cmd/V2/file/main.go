f3d310e9acda69ef09f4aa622002b950eb3a7384
twilight <2359643864@qq.com>
2026-05-09 23:40:59 +0800

    data()

--- cmd/V2/file/main.go
+++ cmd/V2/file/main.go
@@ -25,12 +25,12 @@ var (
 
 func main() {
 	Run(
-		//"archive/logic/engines/UserInterface/SafeCalculate.json",
-		//"archive/logic/engines/UserInterface/SafeCalculate_test.json",
-		//"archive/logic/engines/UserInterface/SetByStep.json",
-		//"archive/logic/engines/UserInterface/SetByStep_test.json",
-		//"archive/logic/engines/UserInterface/CondGroup_Iterator.json",
-		"archive/logic/engines/UserInterface/CondGroup_Iterator_test.json",
+	//"archive/logic/engines/UserInterface/SafeCalculate.json",
+	//"archive/logic/engines/UserInterface/SafeCalculate_test.json",
+	//"archive/logic/engines/UserInterface/SetByStep.json",
+	//"archive/logic/engines/UserInterface/SetByStep_test.json",
+	//"archive/logic/engines/UserInterface/CondGroup_Iterator.json",
+	//"archive/logic/engines/UserInterface/CondGroup_Iterator_test.json",
 	)
 	//RunWithMsgMgr(
 	//
